9258d81df8da2013cf821ecf8e50137f3af040fa
Ahmedou khlil <ouldkhlil09@gmail.com>
2026-03-15 16:40:13 +0000

    recuperation par emplacement et par designation v9

--- resources/views/corbeille/immobilisations/index.blade.php
+++ resources/views/corbeille/immobilisations/index.blade.php
@@ -9,7 +9,33 @@
             </div>
         </div>
 
-        <div x-data="{ open: false }" class="bg-white rounded-lg shadow-sm border border-gray-200">
+        @if(session('success'))
+            <div class="rounded-lg bg-green-50 border border-green-200 p-4">
+                <div class="flex">
+                    <svg class="h-5 w-5 text-green-400 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                    </svg>
+                    <p class="text-sm font-medium text-green-800">{{ session('success') }}</p>
+                </div>
+            </div>
+        @endif
+
+        @if(session('error'))
+            <div class="rounded-lg bg-red-50 border border-red-200 p-4">
+                <div class="flex">
+                    <svg class="h-5 w-5 text-red-400 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                    </svg>
+                    <p class="text-sm font-medium text-red-800">{{ session('error') }}</p>
+                </div>
+            </div>
+        @endif
+
+        @php
+            $hasActiveFilters = !empty($filterDesignation) || !empty($filterEmplacement) || !empty($filterCategorie) || !empty($filterEtat) || !empty($filterNatJur) || !empty($filterSF) || !empty($filterDateAcquisition) || !empty($search);
+        @endphp
+
+        <div x-data="{ open: {{ $hasActiveFilters ? 'true' : 'false' }} }" class="bg-white rounded-lg shadow-sm border border-gray-200">
             <button
                 @click="open = !open"
                 class="w-full px-4 py-3 flex items-center justify-between text-left hover:bg-gray-50 transition-colors"
